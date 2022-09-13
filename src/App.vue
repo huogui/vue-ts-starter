a10160ea01a6360c3e0b31e9be6fd481604be2dc
huogui <645039234@qq.com>
2022-09-13 23:33:45 +0800

    chore: project init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <Button type=""></Button>
+</template>
+
+<style>
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+</style>
+
+<style scoped>
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+defineProps<{ type: 'primary' | 'success' | 'info' }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ type }}</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
